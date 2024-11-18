ac31f3fa70f94927e0d19aff04e3cd7f411870cf
AdityaPawar14 <aadipawar1403@gmail.com>
2024-11-18 08:57:54 +0000

    variables and datatypes

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,16 @@
+const accountID = 114455
+let accountEmail = "aditya1405@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+let accountState;
+
+// accountID = 2
+
+accountEmail = "ap12@gmail.com"
+accountPassword = "140503"
+accountCity = "Kalyan"
+
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
+
+console.log(accountID);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+console.log("Aditya");
+
+let name = "Aditya"
+let age = 22
+let isLoggedIn = true
+
+// number => 2 to the power of 53
+// bigint
+// string => ""
+// boolean => true/false
+//  null => standalone value
+// undefined =>
+// symbol =>unique
+
+
+// object
+
+
+console.log(typeof undefined) // output undefined
+console.log(typeof null) // output object
+

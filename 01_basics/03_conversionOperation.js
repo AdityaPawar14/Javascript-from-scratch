0bd268f249dcb8ff157afe05711f2e95c8b8ecd1
AdityaPawar14 <aadipawar1403@gmail.com>
2024-11-18 09:28:23 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "45"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "45" => 45
+
+// "45abc" => Nan
+// true => 1 ; false => 0
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+//  "any name" => true
+
+let anyNumber = "88"
+
+let stringNumber = string(anyNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+

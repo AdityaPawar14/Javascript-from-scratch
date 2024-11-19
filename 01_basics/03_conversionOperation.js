76d675ee9fa8444ec293deeb9b4ddcf7cc6e54b7
AdityaPawar14 <aadipawar1403@gmail.com>
2024-11-19 07:34:41 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,10 +20,44 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "" => false
 //  "any name" => true
 
-let anyNumber = "88"
+// let anyNumber = "88"
 
-let stringNumber = string(anyNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = string(anyNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
+// *****************************************************  Operations ********************************************************//
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Aditya"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(true);
+// console.log(+true);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+gameCounter++,
+console.log(gameCounter);

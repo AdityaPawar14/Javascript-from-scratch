24ae9325427166b4ba21c5b07e5e979fdb11d0e5
AdityaPawar14 <aadipawar1403@gmail.com>
2024-11-20 10:55:51 +0000

    maths and num

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,38 @@
+const score = 400
+// console.log(score);
+
+const  balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString());
+
+const otherNumber = 128.896   
+
+// console.log(otherNumber.toPrecision(3));
+
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// ++++++++++++++++++++++ Maths +++++++++++++++++++++++++++ //
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.82));
+// console.log(Math.min(5, 6, 2, 1));
+// console.log(Math.max(5, 6, 2, 1));
+
+
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
+console.log((Math.random()*10) + 1);
+
+
+const min = 10
+const max= 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min )

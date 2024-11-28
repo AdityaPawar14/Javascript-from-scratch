9981a240f1f3df5be3085714e7168d120f5da21c
AdityaPawar14 <aadipawar1403@gmail.com>
2024-11-28 08:43:39 +0000

    about control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,60 @@
+///////// if ////////
+const isUserLoggedIn = true
+const temperature = 42
+
+// if (temperature === 40)  {
+//     console.log("less than 50");
+// }else{
+//      console.log("temperature is greater than 50");
+     
+// }
+// console.log("execute");
+
+
+// <, >, <=, >=, ==, !=
+
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`user power: ${power} `);
+// }
+
+// console.log(`user power: ${power} `);
+
+
+// const balance = 5000
+
+// if (balance> 2000) console.log("test"), console.log("test2");
+
+
+
+// if (balance < 2000) {
+//     console.log("less than 1000");
+    
+// }else if(balance < 750) {
+//     console.log("less than 3000");
+    
+// }else if (balance < 4500) {
+//     console.log("less than 3000");
+    
+// }else {
+//     console.log("less than 8000");
+    
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+if (userLoggedIn && debitCard && 2==2) {
+    console.log("Allow to buy courses");
+    
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("user logged in");
+    
+}
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,48 @@
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got user email");
+    
+}else{
+    console.log("dont have user email");
+    
+}
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, Nan 
+
+// truthy values
+
+// "0", 'false', " ", [], {}, function(){}
+
+if (userEmail.length === 0) {
+    console.log("Array is empty");
+    
+}
+
+const emptyobj = {}
+
+if (Object.keys(emptyobj).length === 0){
+    console.log("Object is empty");
+    
+}
+
+// Nullish Coalescing Operator (??) : null, undefiend
+
+let val1;
+// val1 =5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 15
+
+console.log(val1);
+
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
+
